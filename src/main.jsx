b2a993ee2d233b53cae1e03d21741f7259e7b6f4
afsal_ahamad <afzalahmed12598@gmail.com>
2026-09-10 19:29:17 +0530

    Add Vite + React + Tailwind dev environment setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BarakahPOS from '../barakah-pos.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarakahPOS />
+  </React.StrictMode>,
+)
